7e925b05864bedeb03164b651185420d2d4b7f32
blukennel <adrom99@gmail.com>
2018-11-08 16:27:39 +0100

    start

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int menu;
+    int a,b;
+
+    cout<<"1) Dodawanie"<<endl;
+    cout<<"2) Odejmowanie"<<endl;
+    cout<<"3) Mnozenie"<<endl;
+    cout<<"4) Dzielenie"<<endl;
+    cout<<"5) Pierwiastek"<<endl;
+    cout<<"6) Potega"<<endl;
+    cin>>menu;
+    cout<<"Podaj 1 liczbe: ";
+    cin>>a;
+    cout<<"Podaj 2 liczbe: ";
+    cin>>b;
+
+    switch(menu)
+    {
+
+    }
+
+    getchar();
+    return 0;
+}
