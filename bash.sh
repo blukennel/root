b445581719cc1fc2f14b6c66399537a5570a1a35
blukennel <adrom99@gmail.com>
2019-05-04 12:35:57 +0200

    Update

---
+++ bash.sh
@@ -0,0 +1,28 @@
+cd  #idz do /home
+ls #wyswietl
+./file.ext #uruchom
+chmod 777 file.ext #nadaj wszystkie uprawnienia
+ps -a #pokaz procesy
+kill 777777 #zakoncz proces o id 777777
+su user #zmien uzytkownika na USER
+su #zmieni na ROOT
+echo teskt #wyswietl tekst
+echo text >> file.ext #dodaj linijke text do file.ext
+cat file.ext #wyswietl plik
+vim file.ext #edytuje plik
+clear #czysci konsole
+reboot #reset
+gcc file.c -o run #kompiluje file.c do pliku run
+touch file.ext #tworzy nowy plik
+mkdir #tworzy katalog
+rm -r katalog #usuwa caly katalog
+cd ../ #katalog wyzej
+cd / #idz do root
+whoami #podaj nazwe uzytkownika
+rm * #usun wszystkie pliki z katalogu
+chmod 0 file.ext #zablokuj plik
+man program #wyswietla dokumentacje programu
+echo -e #Pomija nawiasy itd
+find *.ext #wyswietle wszytkie pliki z katalogu o rozszerzeniu .ext
+pwd #podaje obecna sciezke
+# CTRL + C #zatrzymaj program
